eabda64927e75598cdd2935c78be2fd7dd633e18
zwhy2024 <thecvcoder@foxmail.com>
2024-07-20 17:54:30 +0800

    update: custom primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: {
+          DEFAULT: "#3ecf8e"
+        }
+      }
     },
   },
   plugins: [nextui()],
