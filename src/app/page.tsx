62fa1d4c8205944077f3341c64c16f58399f64a1
zwhy2024 <thecvcoder@foxmail.com>
2024-07-20 18:21:01 +0800

    update: home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { SignOut } from '@/components/ui/auth-forms/signout'
-import { Button } from '@nextui-org/button'
 
 export default function Home() {
   return (
-    <div>
+    <div className='w-full h-full min-h-dvh bg-stone-100 flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-4xl mb-6'>Your are logged in {'🎉'}</h1>
       <SignOut />
     </div>
   )
