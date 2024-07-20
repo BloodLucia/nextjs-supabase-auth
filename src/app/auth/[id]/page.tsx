b948430be943a4642c115122905c2fcdac18e404
zwhy2024 <thecvcoder@foxmail.com>
2024-07-20 18:14:26 +0800

    add: link to signin or signup

--- src/app/auth/[id]/page.tsx
+++ src/app/auth/[id]/page.tsx
@@ -4,6 +4,8 @@ import { getDefaultAuthView, getViewTyps } from '@/utils/supabase/settings'
 import { PassswordSignIn } from '@/components/ui/auth-forms/password-signin'
 import { SignUp } from '@/components/ui/auth-forms/signup'
 import { createClient } from '@/utils/supabase/server'
+import Image from 'next/image'
+import { Link } from '@nextui-org/link'
 
 export default async function SignIn({ params }: { params: { id: string } }) {
   const viewTypes = getViewTyps()
@@ -27,8 +29,31 @@ export default async function SignIn({ params }: { params: { id: string } }) {
   return (
     <div className="w-full h-full min-h-dvh bg-stone-100 flex flex-col justify-center items-center">
       <div className="max-w-md w-full bg-white shadow-sm rounded-md p-8">
-        {/* <div className="flex justify-center items-center cursor-pointer mb-6"></div> */}
+        <Image
+          width={180}
+          height={0}
+          src="/supabase.svg"
+          alt=""
+          className="mx-auto mb-6 w-[180px] h-auto cursor-pointer"
+        />
         {viewProp === 'password_signin' ? <PassswordSignIn /> : <SignUp />}
+        <div className="text-center text-sm mt-4">
+          {viewProp === 'password_signin' ? (
+            <>
+              Don't have an account?{' '}
+              <Link className="text-sm" href="/auth/signup">
+                Sign up now.
+              </Link>
+            </>
+          ) : (
+            <>
+              Already have an account?{' '}
+              <Link className="text-sm" href="/auth/password_signin">
+                Sign in now.
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   )
